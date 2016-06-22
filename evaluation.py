a4b41d1fb89ab1ecf979c3bcfed39fa6d86161d3
Greg Stoddard <stoddardg@gmail.com>
2016-06-22 11:11:38 -0400

    moving things to .py files

---
+++ evaluation.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from collections import Counter
+def evaluate_bandit(sim, mab, time_steps):
+    reward_list = []
+    arm_play_counter = Counter() 
+    for t in np.arange(time_steps):
+        arms, arm_features = sim.get_available_arms()
+        arm_to_play = mab.get_decision(arms, arm_features)
+        
+        arm_play_counter[arm_to_play] += 1.0
+#         played[i][arm_to_play] += 1
+        
+        reward = sim.get_reward(arm_to_play)
+        mab.update(arm_to_play, reward)
+        reward_list.append(reward*1.0)
+    
+    total_reward = np.sum(reward_list)
+    average_reward_by_time = np.cumsum(reward_list)/np.arange(1,time_steps+1.0)
+    
+    return total_reward, average_reward_by_time
---
+++ simulation.py
@@ -0,0 +1,45 @@
+from scipy.stats import beta
+
+import numpy as np
+import scipy.stats
+
+from collections import Counter
+import sys
+
+# Scales the arm_probs to all be between 0 and 1.
+# The highest pre-scaled value is assigned a value of 1.
+# Probs is a dictionary
+def scale(probs, lower_bound, upper_bound):
+    # min_int
+    biggest = - sys.maxint - 1
+    interval = upper_bound - lower_bound
+    
+    for i in range(len(probs)): 
+        if abs(probs[i]) > biggest:
+            biggest = abs(probs[i])
+    biggest = biggest * 2 / interval
+    for i in range(len(probs)):
+        probs[i] = probs[i]/biggest + (.5*(upper_bound + lower_bound))
+    
+    return probs
+    
+
+class SimpleSimulator:
+    
+    def __init__(self,n_arms,loc_value, scale_value):
+        self.arm_probs = {}
+        temp_val_list = []
+        scaled_vals = []
+        for i in range(0, n_arms):
+            temp_val = np.random.normal(loc=loc_value, scale = scale_value)
+            temp_val_list.append(temp_val)
+        scaled_vals = scale(temp_val_list, lower_bound=0, upper_bound=.2)
+        for i in range(0,n_arms):
+            self.arm_probs[i] = scaled_vals[i]
+    
+    def get_available_arms(self):
+        return self.arm_probs.keys(), []
+    
+    def get_reward(self, arm_id):
+        return scipy.stats.bernoulli.rvs(self.arm_probs.get(arm_id,0))
+    
