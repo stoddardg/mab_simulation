073ccbcba684dec6fc9474b2ba3665657adfae13
Val Leifer <valialeifer@college.harvard.edu>
2016-07-20 11:03:18 -0400

    fixed typo

--- evaluation.py
+++ evaluation.py
@@ -2,10 +2,12 @@ import numpy as np
 
 from collections import Counter
 
-def evaluate_bandit(sim, mab, TIME_STEPS, plays_per_time_step):
+def evaluate_bandit(sim, mab, time_steps, plays_per_time_step):
     reward_list = []
     arm_play_counter = Counter() 
+
     for t in np.arange(time_steps):
+
         arms, arm_features = sim.get_available_arms()
         arm_to_play = mab.get_decision(arms, arm_features)
         
@@ -20,5 +22,7 @@ def evaluate_bandit(sim, mab, TIME_STEPS, plays_per_time_step):
     
     total_reward = np.sum(reward_list)
     average_reward_by_time = np.cumsum(reward_list)/np.arange(1,time_steps+1.0)
+    average_reward_by_time /= (plays_per_time_step*1.0)
+
     
     return total_reward, average_reward_by_time
